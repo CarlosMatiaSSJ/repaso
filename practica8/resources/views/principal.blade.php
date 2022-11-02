7d5954b401ec4e68cbe22bbc2a07816166bf5e3b
CarlosMatiaSSJ <carlosmatias649@gmail.com>
2022-11-02 16:05:58 -0600

    Formulario

--- practica8/resources/views/principal.blade.php
+++ practica8/resources/views/principal.blade.php
@@ -2,8 +2,44 @@
 @section('titulo')
     Principal
 @stop
-
-
 @section('contenido')
+
+{{-- noticia literaria --}}
+
+<h1 class="bold" style="text-align:center mb-3">
+    Annie Ernaux cambió la literatura francesa confesando lo inconfesable", dice Wendy Guerra sobre la ganadora del Nobel de Literatura
+</h1>
+
+<div class="news__excerpt" style="margin-top: 20px">
+    <h3><p>"<strong><a href="https://cnnespanol.cnn.com/2022/10/06/annie-ernaux-nobel-literatura-2022-ganador-trax/">Annie Ernaux</a> </strong>cambió la literatura francesa confesando abortos ilegales, confesando amores prohibidos, confesando lo inconfesable", dice la escritora cubana-francesa Wendy Guerra. "¿Acaso no es eso parte del patrimonio esencial de la literatura femenina contemporánea?", agrega. Mira aquí las reflexiones de Guerra sobre la autoficción de Ernaux, ganadora del Premio Nobel de Literatura 2022.</p></h3>
+</div>
+
+{{-- imagenes --}}
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="{{ asset('imgs/libro1.jpg')}}" class="d-block w-80 h-50" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('imgs/libro2.jpg')}}" class="d-block w-100 h-50" alt="..." >
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('imgs/lirbo3.jpg')}}" class="d-block w-100 h-50" alt="..." >
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
 @stop
--- practica8/resources/views/registrar.blade.php
+++ practica8/resources/views/registrar.blade.php
@@ -2,8 +2,41 @@
 @section('titulo')
     Registrar 
 @stop
-
-
 @section('contenido')
+{{-- Formulario --}}
+<div class="card">
+    <div class="card-body">
+        <form>
+            <div class="mb-3">
+              <label for="isbn" class="form-label">ISBN</label>
+              <input type="text" class="form-control" id="isbn">
+            </div>
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Título</label>
+                <input type="text" class="form-control" id="titulo">
+            </div>
+            <div class="mb-3">
+                <label for="autor" class="form-label">Autor</label>
+                <input type="text" class="form-control" id="autor">
+            </div>
+            <div class="mb-3">
+                <label for="paginas" class="form-label">Páginas</label>
+                <input type="text" class="form-control" id="paginas">
+            </div>
+            <div class="mb-3">
+                <label for="editorial" class="form-label">Editorial</label>
+                <input type="text" class="form-control" id="editorial">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail de la editorial</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+          </form>
+    </div>
+  </div>
+
+
+
 
 @stop
