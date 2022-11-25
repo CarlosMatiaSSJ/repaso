d1063ed800e72e941cb10c99235a0375912d023b
CarlosMatiaSSJ <carlosmatias649@gmail.com>
2022-11-25 10:28:08 -0600

    migraciones creadas

---
+++ practica8/database/migrations/2022_11_25_162733_create__tabla_libros.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_tabla_libros', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_tabla_libros');
+    }
+};
